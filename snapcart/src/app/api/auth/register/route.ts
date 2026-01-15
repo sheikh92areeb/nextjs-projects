2c8dd6719f16a002d9d2ad5ee6883c56a90610d7
Muhammad Areeb <mareebsheikh92@gmail.com>
2026-01-16 00:32:26 +0500

    working

--- snapcart/src/app/api/auth/register/route.ts
+++ snapcart/src/app/api/auth/register/route.ts
@@ -1,9 +1,39 @@
-import { NextRequest } from "next/server";
+import connectDB from "@/lib/db";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req:NextRequest) {
     try {
-        
+        await connectDB()
+        const { name, email, password } = await req.json()
+        const existUser = await User.findOne({email})
+        if (existUser) {
+            return NextResponse.json(
+                {message:"User already exist"},
+                {status:400}
+            )
+        }
+        if (password.length < 6) {
+            return NextResponse.json(
+                {message:"Password must be atleast 6 characters"},
+                {status:400}
+            )
+        }
+        const hashedPassword = await bcrypt.hash(password, 10)
+        const user = await User.create({
+            name,
+            email,
+            password:hashedPassword
+        })
+        return NextResponse.json(
+            user,
+            {status:200}
+        )
     } catch (error) {
-        
+        return NextResponse.json(
+            {message:`Register Error ${error}`},
+            {status:500}
+        )
     }
 }
