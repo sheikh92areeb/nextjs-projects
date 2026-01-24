56c3b28e21ceee76c6d3325dbc7518ed0789b228
Muhammad Areeb <mareebsheikh92@gmail.com>
2026-01-24 19:55:34 +0500

    working

--- snapcart/src/components/UserDashboard.tsx
+++ snapcart/src/components/UserDashboard.tsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import HeroSection from './HeroSection'
+import CategorySlider from './CategorySlider'
 
 function UserDashboard() {
   return (
     <>
       <HeroSection />
+      <CategorySlider />
     </>
   )
 }
