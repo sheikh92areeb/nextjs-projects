958c0bf007d84489032d404a97103da13b42175f
Muhammad Areeb <mareebsheikh92@gmail.com>
2026-05-17 01:21:59 +0500

    working

--- snapcart/src/app/user/checkout/page.tsx
+++ snapcart/src/app/user/checkout/page.tsx
@@ -130,7 +130,7 @@ function Checkout() {
                 paymentMethod
             })
 
-            console.log(result.data)
+            router.push("/user/order-success")
         } catch (error) {
             console.log(error)
         }
---
+++ snapcart/src/app/user/order-success/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function OrderSuccess() {
+  return (
+    <div className='flex flex-col items-center justify-center min-h-[80vh] px-6 text-center bg-linear-to-b from-green-50 to-white'>
+      
+    </div>
+  )
+}
+
+export default OrderSuccess
