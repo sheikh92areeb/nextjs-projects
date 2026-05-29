5ca3c74f25fe1a31a0a360cd982101e30102260b
Muhammad Areeb <mareebsheikh92@gmail.com>
2026-05-29 15:55:03 +0500

    updated

--- 01.snapcart/snapcart/src/app/admin/manage-orders/page.tsx
+++ 01.snapcart/snapcart/src/app/admin/manage-orders/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 import AdminOrderCard from '@/components/AdminOrderCard'
+import { getSocket } from '@/lib/socket'
 import { IOrder } from '@/models/order.model'
 import axios from 'axios'
 import { ArrowLeft, PackageSearch } from 'lucide-react'
@@ -23,6 +24,14 @@ function ManageOrders() {
       }
     }
     getOrders()
+  },[])
+
+  useEffect(() : any =>{
+    const socket = getSocket()
+    socket.on("new-order", (newOrder) => {
+      setOrders(prev => [newOrder, ...prev!])
+    })
+    return () => socket.off("new-order")
   },[])
 
   if (loading) {
--- 01.snapcart/socketServer/index.js
+++ 01.snapcart/socketServer/index.js
@@ -6,6 +6,7 @@ import axios from 'axios'
 
 dotenv.config()
 const app = express()
+app.use(express.json())
 const port = process.env.PORT || 4000
 const server = http.createServer(app)
 
@@ -39,6 +40,17 @@ io.on("connection", (socket) => {
     socket.on("disconnect", () => {
         console.log("User Disconnected", socket.id)
     })
+})
+
+app.post("/notify", (req,res) => {
+    const { event, data, socketId } = req.body
+    if (socketId) {
+        io.to(socketId).emit(event,data)
+    } else {
+        io.emit(event,data)
+    }
+
+    return res.status(200).json({ success: true })
 })
 
 server.listen(port, ()=> {
